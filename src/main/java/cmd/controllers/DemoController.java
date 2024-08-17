287fc8d39f8a70ce12d8dc808cff570c9e51570c
login-nigol <vadim.antipov@gmx.de>
2024-08-17 16:43:02 +0200

    modified comments to DemoController

--- src/main/java/cmd/controllers/DemoController.java
+++ src/main/java/cmd/controllers/DemoController.java
@@ -7,20 +7,25 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController  // используется для обозначения класса как контроллера, обрабатывает HTTP-запросы и возвращает
-// данные в формате JSON или XML.
+                    // данные в формате JSON или XML.
 @RequestMapping("/demo") // используется для задания базового URL для всех методов в контроллере,
-// будут доступны по URL, начинающемуся с /demo.
+                            // будут доступны по URL, начинающемуся с /demo.
 public class DemoController {
 
     @GetMapping("/{id}") // используется для обработки HTTP GET запросов, /{id} указывает, что метод будет
-    // принимать переменную часть URL, которая будет передана в метод в качестве параметра.
+                            // принимать переменную часть URL, которая будет передана в метод в качестве параметра.
     public ResponseEntity<String> getItemById(@PathVariable("id") String value){ // @PathVariable связывает переменную
-        // из URL с параметром метода, значение из {id} в URL будет передано в метод как параметр value.
+                    // из URL с параметром метода, значение из {id} в URL будет передано в метод как параметр value.
         String response = String.format("Передано значение: %s!", value); // Возвращает отформатированную строку,
-        // используя указанную локаль, строку формата и аргументы.
+                                                            // используя указанную локаль, строку формата и аргументы.
         return ResponseEntity.ok(response); // ResponseEntity.ok() создает объект ResponseEntity с HTTP статусом
         // 200 (OK), что указывает на успешное выполнение запроса, ResponseEntity позволяет более гибко управлять
         // HTTP-ответами, включая установку статуса, заголовков и тела ответа.
+    }
+    @PostMapping("/postItem")
+    public ResponseEntity<String> postItemWithValue(@RequestBody String data){
+        String response = String.format("postItem получил данные: %s!", data);
+        return ResponseEntity.ok(response);
     }
 
     // @RestController
