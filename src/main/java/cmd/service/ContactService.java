58ec6dd4fb3e2a91ff2df7021d6c104e381d567b
login-nigol <vadim.antipov@gmx.de>
2024-08-20 20:12:32 +0200

    updated ContactService

--- src/main/java/cmd/service/ContactService.java
+++ src/main/java/cmd/service/ContactService.java
@@ -2,17 +2,34 @@ package cmd.service;
 
 import java.util.UUID;
 import cmd.model.Contacts;
+import cmd.repositories.LocalContactsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service // обозначения класса как сервисного
 public class ContactService {
+    @Autowired
+    private LocalContactsRepository localContactsRepository;
 
-    public Contacts getContact() { // ручка
-        return Contacts.builder()
-                .id(UUID.randomUUID())
-                .name("Вася Пупкин")
-                .email("василий@пуп.ру")
-                .build();
+    public ContactService(LocalContactsRepository localContactsRepository) {
+        this.localContactsRepository = localContactsRepository;
     }
+    public Contacts getAllContact() { // ручка
+        return localContactsRepository.findAll();
+    }
+
 }
+
+
+//@Service // обозначения класса как сервисного
+//public class ContactService {
+//
+//    public Contacts getContact() { // ручка
+//        return Contacts.builder()
+//                .id(UUID.randomUUID())
+//                .name("Вася Пупкин")
+//                .email("василий@пуп.ру")
+//                .build();
+//    }
+//}
